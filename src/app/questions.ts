657c61d5da722f7d4d90a468cf8aee410a209a3b
Gabriel Feijó <92588394+GabrielFeijo@users.noreply.github.com>
2023-12-20 10:22:17 -0300

    feat: list of questions

---
+++ src/app/questions.ts
@@ -0,0 +1,34 @@
+export const questions = [
+	{
+		id: 1,
+		title: 'O que é HTML e qual é o seu papel no desenvolvimento front-end?',
+		answer:
+			'HTML, que significa Linguagem de Marcação de Hipertexto (Hypertext Markup Language), é a espinha dorsal do desenvolvimento front-end. Ele é utilizado para estruturar o conteúdo de uma página web, definindo elementos como cabeçalhos, parágrafos, listas e links. Em suma, HTML cria a estrutura básica de uma página web.',
+	},
+	{
+		id: 2,
+		title: 'Por que o CSS é importante no desenvolvimento front-end?',
+		answer:
+			'CSS, ou Cascading Style Sheets, desempenha um papel crucial no desenvolvimento front-end ao permitir o design e a estilização de páginas web. Ele controla a apresentação visual, incluindo layout, cores, fontes e espaçamento. Em resumo, o CSS torna as páginas web visualmente atraentes e responsivas.',
+	},
+	{
+		id: 3,
+		title:
+			'Como o JavaScript contribui para a interatividade em uma página web?',
+		answer:
+			'JavaScript é uma linguagem de programação amplamente utilizada no desenvolvimento front-end para adicionar interatividade dinâmica às páginas web. Ele permite a manipulação de elementos HTML, resposta a eventos do usuário e a atualização dinâmica do conteúdo, proporcionando uma experiência mais envolvente aos visitantes do site.',
+	},
+	{
+		id: 4,
+		title: 'O que são frameworks front-end e por que eles são usados?',
+		answer:
+			'Frameworks front-end, como React, Vue.js e Angular, são conjuntos de ferramentas e bibliotecas que simplificam o desenvolvimento de aplicações web. Eles oferecem estruturas organizadas e padrões de código que facilitam a construção de interfaces complexas, melhoram a manutenção do código e promovem a reutilização de componentes.',
+	},
+	{
+		id: 5,
+		title:
+			'Qual é a importância do design responsivo no desenvolvimento front-end?',
+		answer:
+			'O design responsivo é crucial no desenvolvimento front-end porque garante que uma aplicação ou site seja visualmente atraente e funcional em diferentes dispositivos e tamanhos de tela. Isso melhora a experiência do usuário, pois o conteúdo se ajusta automaticamente para proporcionar uma navegação suave, independentemente do dispositivo utilizado.',
+	},
+];
